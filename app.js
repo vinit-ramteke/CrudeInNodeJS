9fc05e5adbc81eb50f7d9ef09201d071f66f4ab7
RaginiBawankar <ragini714bawankar@gmail.com>
2024-08-01 19:14:36 +0530

    ragini 01/08/2024

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ app.get('/', (req, res) => {
 app.get('/abc', (req, res) => {
     res.render('index');
 });
+
+
+app.get('/xyz', (req, res) => {
+    res.render('index');
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
