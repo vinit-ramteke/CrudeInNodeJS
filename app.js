29331e5725a07139f0b0503081878d4b94763fc4
vinit-ramteke <vinit.ramteke@gmail.com>
2024-08-01 19:01:24 +0530

    01/08/2024 _1

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.get('/abc', (req, res) => {
+    res.render('index');
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
